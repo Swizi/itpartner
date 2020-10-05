0c02cd3cb8551192601a7af9495aa36267dbaeb9
Swizi <denis.bosiy@gmail.com>
2020-10-05 23:50:45 +0300

    Added shadow card to list

--- src/panels/SearchPage.js
+++ src/panels/SearchPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
-import { platform, IOS } from "@vkontakte/vkui";
+import { platform, IOS, Separator } from "@vkontakte/vkui";
 import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
 import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
@@ -21,6 +21,8 @@ import Icon28AddOutline from "@vkontakte/icons/dist/28/add_outline";
 import Icon24AddOutline from "@vkontakte/icons/dist/24/add_outline";
 import axios from ".././axios";
 import Text from "@vkontakte/vkui/dist/components/Typography/Text/Text";
+import Title from "@vkontakte/vkui/dist/components/Typography/Title/Title";
+import Subhead from "@vkontakte/vkui/dist/components/Typography/Subhead/Subhead";
 import Icon28SettingsOutline from "@vkontakte/icons/dist/28/settings_outline";
 import Icon24Settings from "@vkontakte/icons/dist/24/settings";
 
@@ -34,6 +36,8 @@ import FormLayoutGroup from "@vkontakte/vkui/dist/components/FormLayoutGroup/For
 import View from "@vkontakte/vkui/dist/components/View/View";
 
 import Link from "@vkontakte/vkui/dist/components/Link/Link";
+import Card from "@vkontakte/vkui/dist/components/Card/Card";
+import CardGrid from "@vkontakte/vkui/dist/components/CardGrid/CardGrid";
 import Spinner from "@vkontakte/vkui/dist/components/Spinner/Spinner";
 
 import "./SearchPage.css";
@@ -48,7 +52,6 @@ const SearchPage = (props) => {
   const [pageYOffset, setPageYOffset] = useState(0);
   const [page, setPage] = useState(0);
 
-  
   const [fullPartners, setFullPartners] = useState([]);
   const [filteredPartners, setFilteredPartners] = useState([]);
 
@@ -83,42 +86,66 @@ const SearchPage = (props) => {
     if (props.activeModal === null) {
       var ex_partners = fullPartners;
       if (props.filterOptions.design === true) {
-        ex_partners = ex_partners.filter(function (filteredPartner, i, ex_partners) {
+        ex_partners = ex_partners.filter(function (
+          filteredPartner,
+          i,
+          ex_partners
+        ) {
           if (filteredPartner.design === true) {
             return ex_partners[i];
           }
         });
       }
       if (props.filterOptions.frontend === true) {
-        ex_partners = ex_partners.filter(function (filteredPartner, i, ex_partners) {
+        ex_partners = ex_partners.filter(function (
+          filteredPartner,
+          i,
+          ex_partners
+        ) {
           if (filteredPartner.frontend === true) {
             return ex_partners[i];
           }
         });
       }
       if (props.filterOptions.backend === true) {
-        ex_partners = ex_partners.filter(function (filteredPartner, i, ex_partners) {
+        ex_partners = ex_partners.filter(function (
+          filteredPartner,
+          i,
+          ex_partners
+        ) {
           if (filteredPartner.backend === true) {
             return ex_partners[i];
           }
         });
       }
       if (props.filterOptions.mobile === true) {
-        ex_partners = ex_partners.filter(function (filteredPartner, i, ex_partners) {
+        ex_partners = ex_partners.filter(function (
+          filteredPartner,
+          i,
+          ex_partners
+        ) {
           if (filteredPartner.mobile === true) {
             return ex_partners[i];
           }
         });
       }
       if (props.filterOptions.desktop === true) {
-        ex_partners = ex_partners.filter(function (filteredPartner, i, ex_partners) {
+        ex_partners = ex_partners.filter(function (
+          filteredPartner,
+          i,
+          ex_partners
+        ) {
           if (filteredPartner.desktop === true) {
             return ex_partners[i];
           }
         });
       }
       if (props.filterOptions.city !== "") {
-        ex_partners = ex_partners.filter(function (filteredPartner, i, ex_partners) {
+        ex_partners = ex_partners.filter(function (
+          filteredPartner,
+          i,
+          ex_partners
+        ) {
           if (filteredPartner.city.id.toString() === props.filterOptions.city) {
             return ex_partners[i];
           }
@@ -133,41 +160,43 @@ const SearchPage = (props) => {
       });
       if (searchValue !== "") {
         var userVal = searchValue;
-        userVal = userVal.replace(/\s/g, '');
-        ex_partners = 
-          ex_partners.filter(function (filteredPartner, i, arrayPartners) {
-            var first_last_name = filteredPartner.first_name + filteredPartner.last_name;
-            var last_first_name = filteredPartner.last_name + filteredPartner.first_name;
-            if (
-              !(
-                filteredPartner.first_name
-                  .toLowerCase()
-                  .indexOf(userVal.toLowerCase()) === -1
-              ) ||
-              !(
-                filteredPartner.last_name
-                  .toLowerCase()
-                  .indexOf(userVal.toLowerCase()) === -1
-              ) ||
-              !(
-                first_last_name
-                  .toLowerCase()
-                  .indexOf(userVal.toLowerCase()) === -1
-              ) ||
-              !(
-                last_first_name
-                  .toLowerCase()
-                  .indexOf(userVal.toLowerCase()) === -1
-              )
-            ) {
-              return arrayPartners[i];
-            }
-          })
+        userVal = userVal.replace(/\s/g, "");
+        ex_partners = ex_partners.filter(function (
+          filteredPartner,
+          i,
+          arrayPartners
+        ) {
+          var first_last_name =
+            filteredPartner.first_name + filteredPartner.last_name;
+          var last_first_name =
+            filteredPartner.last_name + filteredPartner.first_name;
+          if (
+            !(
+              filteredPartner.first_name
+                .toLowerCase()
+                .indexOf(userVal.toLowerCase()) === -1
+            ) ||
+            !(
+              filteredPartner.last_name
+                .toLowerCase()
+                .indexOf(userVal.toLowerCase()) === -1
+            ) ||
+            !(
+              first_last_name.toLowerCase().indexOf(userVal.toLowerCase()) ===
+              -1
+            ) ||
+            !(
+              last_first_name.toLowerCase().indexOf(userVal.toLowerCase()) ===
+              -1
+            )
+          ) {
+            return arrayPartners[i];
+          }
+        });
       }
       setFilteredPartners(ex_partners);
     }
   }, [props.activeModal, searchValue]);
-
 
   const handleScroll = (event) => {
     const { scrollTop, clientHeight, scrollHeight } = event.currentTarget;
@@ -196,67 +225,116 @@ const SearchPage = (props) => {
       >
         <Div>
           {filteredPartners.map((partner, index) => (
-            <RichCell
-              key={index}
-              disabled
-              multiline
-              style={{
-                display:
-                  partner.show_user === true ||
-                  partner.vk_id === props.userInfo.vk_id
-                    ? "flex"
-                    : "none",
-              }}
-              after={
-                partner.vk_id === props.userInfo.vk_id ? (
-                  osName === IOS ? (
-                    <Icon28SettingsOutline
-                      data-to="user_info"
-                      onClick={props.go}
-                    />
-                  ) : (
-                    <Icon24Settings data-to="user_info" onClick={props.go} />
-                  )
-                ) : null
-              }
-              before={<Avatar size={72} src={partner.photo} />}
-              text={
-                <Link
-                  style={{
-                    display: partner.website !== "" ? "block" : "none",
-                  }}
-                  href={`${
-                    partner.website !== "" ? `https://${partner.website}` : ""
-                  }`}
-                  target="_blank"
-                >
-                  Сайт-портфолио
-                </Link>
-              }
-              caption={`${partner.design ? "Дизайн" : ""} ${
-                partner.frontend ? "Фронтенд" : ""
-              } ${partner.backend ? "Бэкэнд" : ""} ${
-                partner.mobile ? "Мобайл" : ""
-              } ${partner.desktop ? "Десктоп" : ""}`}
-              actions={
-                <React.Fragment>
-                  <Button
-                    mode="primary"
-                    href={`https://vk.com/id${partner.vk_id}`}
-                    target="__blank"
-                  >
-                    Зайти в вк
-                  </Button>
-                </React.Fragment>
-              }
-            >
-              {partner.first_name} {partner.last_name}{" "}
-              {partner.vk_id === props.userInfo.vk_id
-                ? partner.show_user
-                  ? "(Виден всем)"
-                  : "(Виден только Вам)"
-                : null}
-            </RichCell>
+            <CardGrid>
+              <Card size="l" mode="shadow">
+                <div className="card_wrapper">
+                  <div className="user_info-block">
+                    <Avatar size={72} src={partner.photo} />
+                    <div style={{ marginLeft: 10 }}>
+                      <Title
+                        level="2"
+                        weight="semibold"
+                        style={{ marginBottom: 12 }}
+                      >
+                        {partner.first_name} {partner.last_name}
+                      </Title>
+                      <Subhead weight="medium" style={{ marginBottom: 5 }}>
+                        {partner.design ? "Дизайн " : ""}
+                        {partner.frontend ? "Фронтенд " : ""}
+                        {partner.backend ? "Бэкэнд " : ""}
+                        {partner.mobile ? "Мобайл " : ""}
+                        {partner.desktop ? "Десктоп" : ""}
+                      </Subhead>
+                      <Link
+                        style={{
+                          display: partner.website !== "" ? "block" : "none",
+                        }}
+                        href={`${
+                          partner.website !== ""
+                            ? `https://${partner.website}`
+                            : ""
+                        }`}
+                        target="_blank"
+                      >
+                        Сайт-портфолио
+                      </Link>
+                    </div>
+                  </div>
+                  <div className="user_connect_block">
+                    {partner.vk_id === props.userInfo.vk_id ? <Button mode="secondary" data-to="user_info" onClick={props.go}>Настройки</Button> : null}
+                    <Button
+                      className="enter_vk_button"
+                      mode="primary"
+                      href={`https://vk.com/id${partner.vk_id}`}
+                      target="__blank"
+                    >
+                      Зайти в вк
+                    </Button>
+                  </div>
+                </div>
+              </Card>
+            </CardGrid>
+            // <RichCell
+            //   key={index}
+            //   disabled
+            //   multiline
+            //   style={{
+            //     display:
+            //       partner.show_user === true ||
+            //       partner.vk_id === props.userInfo.vk_id
+            //         ? "flex"
+            //         : "none",
+            //   }}
+            //   after={
+            //     partner.vk_id === props.userInfo.vk_id ? (
+            //       osName === IOS ? (
+            //         <Icon28SettingsOutline
+            //           data-to="user_info"
+            //           onClick={props.go}
+            //         />
+            //       ) : (
+            //         <Icon24Settings data-to="user_info" onClick={props.go} />
+            //       )
+            //     ) : null
+            //   }
+            //   before={<Avatar size={72} src={partner.photo} />}
+            //   text={
+            //     <Link
+            //       style={{
+            //         display: partner.website !== "" ? "block" : "none",
+            //       }}
+            //       href={`${
+            //         partner.website !== "" ? `https://${partner.website}` : ""
+            //       }`}
+            //       target="_blank"
+            //     >
+            //       Сайт-портфолио
+            //     </Link>
+            //   }
+            //   caption={`${partner.design ? "Дизайн" : ""} ${
+            //     partner.frontend ? "Фронтенд" : ""
+            //   } ${partner.backend ? "Бэкэнд" : ""} ${
+            //     partner.mobile ? "Мобайл" : ""
+            //   } ${partner.desktop ? "Десктоп" : ""}`}
+            //   actions={
+            //     <React.Fragment>
+            //       <Button
+            //         mode="primary"
+            //         href={`https://vk.com/id${partner.vk_id}`}
+            //         target="__blank"
+            //       >
+            //         Зайти в вк
+            //       </Button>
+            //     </React.Fragment>
+            //   }
+            // >
+            //   {partner.first_name} {partner.last_name}{" "}
+            //   {partner.vk_id === props.userInfo.vk_id
+            //     ? partner.show_user
+            //       ? "(Виден всем)"
+            //       : "(Виден только Вам)"
+            //     : null}
+            // </RichCell>
           ))}
           {loading && <Spinner size="medium" style={{ marginTop: 20 }} />}
         </Div>
